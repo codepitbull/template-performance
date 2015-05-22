b871b4888aee6dad3471ccf3ef8c1a9afcadb443
jochen <666jochen666>
2015-05-22 13:54:53 +0200

    added freemarker and some more docs

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 A small project comparing performance of different templating engines for a very simple usecase.
+
+Tested engines:
+
+(freemarker)[http://freemarker.org/]
+
+(mustache)[https://github.com/spullara/mustache.java]
+
+(velocity)[http://velocity.apache.org/]
+
+(StringTemplate)[http://www.stringtemplate.org/]
+
+
+_Please note: this test is done with ONLY OUR requirements in mind, your mileage may vary_
+
+Our requirements were:
+
+- Renders plain text templates (that's why we didn't use thymeleaf)
+- uses templates from String
+- doesn't require a web container (messages are rendered inside Vert.x and propagated using the event bus)
+
+
+Numbers in ms for 10000000 renderings on my machine (MBP 13" 2014):
+
+- FreemarkerTest elapsed 3711
+- MustacheTest elapsed 865
+- StringTemplateTest elapsed 10264
+- VelocityTest elapsed 3328
---
+++ src/test/java/de/codepitbull/template/FreemarkerTest.java
@@ -0,0 +1,38 @@
+package de.codepitbull.template;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.junit.Test;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static freemarker.template.Configuration.VERSION_2_3_22;
+
+/**
+ * Created by jmader on 22.05.15.
+ */
+public class FreemarkerTest {
+    @Test
+    public void testFreemarker() throws Exception{
+        String templateStr="$val1 and $val2 and $val3";
+        Template t = new Template("name", new StringReader(templateStr),
+                new Configuration(VERSION_2_3_22));
+        Map<String, String> values = new HashMap<>();
+        values.put("val1", "value1");
+        values.put("val2", "value2");
+        values.put("val3", "value3");
+
+        long start = System.currentTimeMillis();
+        for (int count = 0; count < 10_000_000; count++) {
+            StringWriter sw = new StringWriter();
+            t.process(values, sw);
+            if(sw.toString().length()<0)
+                throw new RuntimeException();
+        }
+        long stop = System.currentTimeMillis();
+        System.out.println(FreemarkerTest.class.getSimpleName() + " elapsed " + (stop - start));
+    }
+}
